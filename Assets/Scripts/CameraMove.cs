3325065e9c98e5a8b43c7c3f361cb60c518f51ab
Kozhevnikov Dmitriy <kozhevnikov.dmitriy@sonakai.com>
2025-12-23 10:10:29 +0400

    - visual changes for bookshelfs - add some textures - improve for camera move - add normal shift for book spawn

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -1,11 +1,14 @@
+using System;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    const float cameraMoveSpeed = .03f;
-    const float cameraScrollSpeed = .5f;
-    const float maxValueZoom = 15.0f;
-    const float minValueZoom = 1.5f;
+    private const float cameraMoveSpeed = .03f;
+    private const float cameraScrollSpeed = .5f;
+    private const float maxValueZoom = 15.0f;
+    private const float minValueZoom = 1.5f;
+    private const float maxValueMoveX = 25.0f;
+    private const float maxValueMoveY = 7.0f;
     private CameraInputActions input;
 
     private void Awake()
@@ -26,15 +29,19 @@ public class CameraMove : MonoBehaviour
     private void Update()
     {
         Camera cam = GetComponent<Camera>();
+        Vector3 pos = this.transform.position;
         float zoom = input.Camera.Zoom.ReadValue<float>();
-        
+
         if (input.Camera.Click.IsPressed())
         {
             Vector3 delta = input.Camera.Drag.ReadValue<Vector2>();
-            this.transform.transform.position -= delta * cam.orthographicSize * cameraMoveSpeed;
+            pos -= delta * cam.orthographicSize * cameraMoveSpeed;
+            
+            pos.x = Math.Clamp(pos.x, -maxValueMoveX, maxValueMoveX);
+            pos.y = Math.Clamp(pos.y, -maxValueMoveY, maxValueMoveY);
+            this.transform.position = pos;
         }
-
-        cam.orthographicSize -= zoom * cameraScrollSpeed;
-        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, minValueZoom, maxValueZoom);
+        
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - zoom * cameraScrollSpeed, minValueZoom, maxValueZoom);
     }
 }
--- Assets/Scripts/CreateBookshelfs.cs
+++ Assets/Scripts/CreateBookshelfs.cs
@@ -27,6 +27,7 @@ public class CreateBookshelfs : MonoBehaviour
     private SQLite.SQLiteConnection db;
     private LibraryManager libraryManager = new LibraryManager();
 
+    private float shiftPosition = .0f;
 
     private void Awake()
     {
@@ -35,39 +36,29 @@ public class CreateBookshelfs : MonoBehaviour
 
     private void Start()
     {
-        if (IsAddNewBook)
-        {
-            libraryManager.AddBook(db, "Обитель апельсинового дерева", 2, 2019, true, 5, 800, 20);
-        }
-
-        if (IsAddNewAuthor)
-        {
-            libraryManager.AddAuthor(db, "Саманта", "Шеннон");
-        }
-
-        if (IsAddNewGenre)
-        {
-            libraryManager.AddGenre(db, "Фэнтези");
-        }
+        //DELETE
+        AddDatasToTables();
 
         List<Book> booksList = libraryManager.GetBooksTitles(db);
 
         SpawnPoints placesForBooksGetSpawn = null;
+        // GameObject placeForBooks = null;
         int countBooksInPlace = 0;
 
         foreach (var placeOnScene in placesForBooksList)
         {
             placesForBooksGetSpawn = placeOnScene.GetComponent<SpawnPoints>();
 
-            for (int i = 0; i < placesForBooksGetSpawn.SpawnPointsList.Count; i++)
+            foreach (var spawnPoint in placesForBooksGetSpawn.SpawnPointsList)
             {
-                countBooksInPlace += placesForBooksGetSpawn.SpawnPointsList[i].booksCount;
+                countBooksInPlace += spawnPoint.booksCount;
             }
 
             //TODO Надо бы ловить ошибку, если лимит будет привышен. Что в таком случае делать?
             //Больше мест для книг или ставить ограничение.
             if (booksList.Count <= countBooksInPlace)
             {
+                // placeForBooks = GameObject.Instantiate(placeOnScene, room);
                 placeOnScene.SetActive(true);
                 break;
             }
@@ -77,25 +68,23 @@ public class CreateBookshelfs : MonoBehaviour
 
         int countBooksInPoint = 0;
         int booksCount = 0;
-        float shiftPosition = .0f;
 
+        // foreach (var point in placeForBooks.GetComponent<SpawnPoints>().SpawnPointsList)
         foreach (var point in placesForBooksGetSpawn.SpawnPointsList)
         {
-            countBooksInPoint = point.booksCount;
             shiftPosition = .0f;
-
+            countBooksInPoint = point.booksCount;
             while (countBooksInPoint > 0 && booksCount < booksList.Count)
             {
-                shiftPosition += .3f;
                 // BookGenerator(point.point, booksList[booksCount].Title, Random.Range(5, 1000), Random.Range(5, 30), shiftPosition);
-                BookGenerator(point.point, booksList[booksCount].Title, booksList[booksCount].PageCount, booksList[booksCount].BookHight, shiftPosition);
+                BookGenerator(point.point, booksList[booksCount].Title, booksList[booksCount].PageCount, booksList[booksCount].BookHight);
                 countBooksInPoint--;
                 booksCount++;
             }
         }
     }
 
-    private void BookGenerator(GameObject currentBookshelf, string bookName = "Book Name", float pageCount = 500, float hight = 20, float shiftPosition = .0f)
+    private void BookGenerator(GameObject currentBookshelf, string bookName = "Book Name", float pageCount = 500, float hight = 20)
     {
         float bookWidth = 0;
         float bookHeight = 0;
@@ -110,6 +99,7 @@ public class CreateBookshelfs : MonoBehaviour
 
         bookWidth = pageCount/5000;
         bookHeight = hight/25;
+        shiftPosition += bookWidth/2;
 
         GameObject book = GameObject.Instantiate(bookPrefab, currentBookshelf.transform);
         book.GetComponent<BoxCollider2D>().size = new Vector2(bookWidth, bookHeight);
@@ -118,5 +108,28 @@ public class CreateBookshelfs : MonoBehaviour
         book.transform.GetChild(1).GetComponent<RectTransform>().sizeDelta = new Vector2(bookHeight - ((20.0f / 100.0f) * bookHeight), bookWidth - ((20.0f / 100.0f) * bookWidth));
         book.transform.GetChild(1).GetComponent<TextMeshPro>().text = bookName;
         // book.transform.localRotation = Quaternion.Euler(0, 0, 45f);
+        shiftPosition += bookWidth/2;
+    }
+
+    private void AddDatasToTables()
+    {
+        if (IsAddNewBook)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                libraryManager.AddBook(db, "Тест", 3, Random.Range(1930, 2025), false, Random.Range(0, 5), Random.Range(400, 800), Random.Range(15, 23));
+            }
+            // libraryManager.AddBook(db, "Обитель апельсинового дерева", 2, 2019, true, 5, 800, 20);
+        }
+
+        if (IsAddNewAuthor)
+        {
+            libraryManager.AddAuthor(db, "Тест", "Тестович");
+        }
+
+        if (IsAddNewGenre)
+        {
+            libraryManager.AddGenre(db, "Фэнтези");
+        }
     }
 }
